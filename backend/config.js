4702cbf4d9f6ba7611d7d40b239ca3929dacc9b7
Ashley <ashleylobo2001@gmail.com>
2024-09-22 19:19:24 +0530

    Completed

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+  'mongodb+srv://ashleylobo2001:ash123@bookstoreit.ayi0s.mongodb.net/?retryWrites=true&w=majority&appName=bookstoreIT';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
